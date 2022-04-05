4d9a6edb9120c8a8087c115572a9e675472cd94f
martame18 <martamaleno.185@hotmail.com>
2022-04-05 09:02:55 +0200

    #patch añadido super a toString

--- source/eburyApp/eburyApp-jpa/src/main/java/es/uma/softcoders/eburyApp/CuentaFintech.java
+++ source/eburyApp/eburyApp-jpa/src/main/java/es/uma/softcoders/eburyApp/CuentaFintech.java
@@ -145,7 +145,7 @@ public class CuentaFintech extends Cuenta{
 
 	@Override
 	public String toString() {
-		return "CuentaFintech = {\n\testado: " + estado + ",\n\tfechaApertura: " + fechaApertura + 
+		return "CuentaFintech = {\n\t"+ super.toString() +"\n\testado: " + estado + ",\n\tfechaApertura: " + fechaApertura + 
 				",\n\tfechaCierre: " + fechaCierre + ",\n\tclasificacion: " + clasificacion + 
 				",\n\tcliente: " + cliente + "\n}";
 	}
