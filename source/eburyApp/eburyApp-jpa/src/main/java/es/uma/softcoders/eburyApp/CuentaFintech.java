5fa161634c84224de33987721dcae6ee2fc7e304
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-04-07 20:18:41 +0200

    Borrado import en desuso en CuentaFintech #none

--- source/eburyApp/eburyApp-jpa/src/main/java/es/uma/softcoders/eburyApp/CuentaFintech.java
+++ source/eburyApp/eburyApp-jpa/src/main/java/es/uma/softcoders/eburyApp/CuentaFintech.java
@@ -8,7 +8,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name="CUENTA_FINTECH")
