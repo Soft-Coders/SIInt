de1e4e89eda7ebf6bec28a4f47c67b135d0cdbda
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-04-23 20:36:35 +0200

    Lógica principal de informeHolanda() @patch

--- source/eburyApp/eburyApp-ejb/src/main/java/es/uma/softcoders/eburyApp/ejb/InformesEJB.java
+++ source/eburyApp/eburyApp-ejb/src/main/java/es/uma/softcoders/eburyApp/ejb/InformesEJB.java
@@ -10,23 +10,63 @@ import es.uma.softcoders.eburyApp.exceptions.InvalidJSONQueryException;
 @Stateless
 public class InformesEJB implements Informes{
 	
+	/**
+	 * Método encargado de responder a la consulta de la API REST para los informes de Holanda.
+	 * 
+	 * @throws InvalidJSONQueryException
+	 * @param json String que contiene la consulta de la API en formato JSON
+	 * @author Ignacio Lopezosa
+	 */
 	@Override
 	public void informeHolanda(String json) throws InvalidJSONQueryException {
 		Object file = JSONValue.parse(json);
 		JSONObject jsonObj = (JSONObject) file;
 		
+		// Buscar "searchParameters"
 		Object searchParameters = jsonObj.get("searchParameters");
 		if(searchParameters == null)
 			throw new InvalidJSONQueryException("searchParameters NOT FOUND");
-		
 		JSONObject spObj = (JSONObject) searchParameters;
 		
+		// Buscar "questionType"
 		String questionType = (String) spObj.get("questionType");
 		if(questionType == null)
 			throw new InvalidJSONQueryException("questionType NOT FOUND");
 		
+		// Elegir funcionamiento según "questionType"
+		switch(questionType) {
+		case "Product":
+			product(spObj);
+			break;
+		case "Customer":
+			customer(spObj);
+			break;
+		default:
+			throw new InvalidJSONQueryException("questionType NOT VALID");	
+		}
+	}
+
+	/** Método privado que contiene la lógica para gestión de consultas de <b>Cuentas</b> a través de API REST. Usado en informes de Holanda.
+	 * 
+	 * @param searchParameters Objeto JSON que contiene los datos de la consulta
+	 * @author Ignacio Lopezosa
+	 * */
+	private void product(JSONObject searchParameters) {
+		// TODO Auto-generated method stub
+		
 	}
 	
+	/**
+	 * Método privado que contiene la lógica para gestión de consultas de <b>Clientes</b> a través de API REST. Usado en informes de Holanda.
+	 * 
+	 * @param searchParameters Objeto JSON que contiene los datos de la consulta
+	 * @author Ignacio Lopezosa
+	 */
+	private void customer(JSONObject searchParameters) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public void informeAlemaniaInicio() {
 		// TODO Auto-generated method stub
