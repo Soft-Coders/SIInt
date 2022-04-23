898d236c2341dbc8654e542370f425a8faf475d0
Ignacio Lopezosa Serrano <n4xo@pm.me>
2022-04-24 00:32:10 +0200

    Terminado informeHolanda() #minor

--- source/eburyApp/eburyApp-ejb/src/main/java/es/uma/softcoders/eburyApp/ejb/InformesEJB.java
+++ source/eburyApp/eburyApp-ejb/src/main/java/es/uma/softcoders/eburyApp/ejb/InformesEJB.java
@@ -1,5 +1,6 @@
 package es.uma.softcoders.eburyApp.ejb;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -66,16 +67,16 @@ public class InformesEJB implements Informes{
 	 * */
 	private List<Object> product(JSONObject spObj) throws InvalidJSONQueryException {
 		
-		String hql = "FROM CuentaFintech CF";
-		String status = (String) spObj.get("status");
+		String hql           = "FROM CuentaFintech CF";
+		String status        = (String) spObj.get("status");
 		String productNumber = (String) spObj.get("productNumber");
 		
 		if(status != null || productNumber != null) {
 			hql.concat(" WHERE ");	
-			int queryLength = hql.length();			// Longitud de query antes de WHERE
+			int queryLength = hql.length();
 			
 			if(status.equalsIgnoreCase("active"))
-				hql.concat("CF.estado = 'ACTIVO'");	//TODO Determinar nomenclatura de CuentaFintech.estado
+				hql.concat("CF.estado = 'ACTIVO'");		//TODO Determinar nomenclatura de CuentaFintech.estado
 			else if(status.equalsIgnoreCase("inactive"))
 				hql.concat("CF.estado = 'INACTIVO'");	//TODO Determinar nomenclatura de CuentaFintech.estado
 			else
@@ -89,6 +90,7 @@ public class InformesEJB implements Informes{
 		}
 		
 		Query query = em.createQuery(hql);
+		@SuppressWarnings("unchecked")
 		List<Object> results = query.getResultList();
 		return results;
 	}
@@ -99,11 +101,110 @@ public class InformesEJB implements Informes{
 	 * @param searchParameters Objeto JSON que contiene los datos de la consulta
 	 * @author Ignacio Lopezosa
 	 * @return Lista de las <b>Clientes</b>que cumplen las condiciones de la consulta
+	 * @throws InvalidJSONQueryException 
 	 */
-	private List<Object> customer(JSONObject spObj) {
-		return null;
-		// TODO Auto-generated method stub
+	@SuppressWarnings("deprecation")
+	private List<Object> customer(JSONObject spObj) throws InvalidJSONQueryException {
 		
+		String hql         = "FROM Cliente C WHERE ";
+		int queryLength    = hql.length();
+		String startPeriod = (String) spObj.get("startPeriod");
+		String endPeriod   = (String) spObj.get("endPeriod");
+		JSONObject name    = (JSONObject) spObj.get("name");
+		String firstName   = (String) name.get("fisrtName");
+		String lastName    = (String) name.get("lastName");
+		JSONObject address = (JSONObject) spObj.get("address");
+		String street      = (String) address.get("street");	// incluye calle y número
+		String city        = (String) address.get("city");
+		String postalCode  = (String) address.get("postalCode");
+		String country     = (String) address.get("country");
+		
+		if(startPeriod != null) 
+			hql.concat("C.fechaAlta = :startPeriod");
+		if(endPeriod != null) {
+			if(hql.length() > queryLength)
+				hql.concat(" AND ");
+			hql.concat("C.fechaBaja = :endPeriod");
+		}
+		if(name != null) {
+			if(firstName != null) {
+				if(hql.length() > queryLength)
+					hql.concat(" AND ");
+				hql.concat("C.nombre = '" + firstName + "'");
+			}
+			if(lastName != null) {
+				if(hql.length() > queryLength)
+					hql.concat(" AND ");
+				hql.concat("C.apellido = '" + lastName + "'");
+			}
+		}
+		if(address != null) {
+			if(street != null) {
+				if(hql.length() > queryLength)
+					hql.concat(" AND ");
+				hql.concat("C.direccion = '" + street + "'");
+			}
+			if(city != null) {
+				if(hql.length() > queryLength)
+					hql.concat(" AND ");
+				hql.concat("C.ciudad = '" + city + "'");
+			}
+			if(postalCode != null) {
+				if(hql.length() > queryLength)
+					hql.concat(" AND ");
+				hql.concat("C.codigoPostal = " + postalCode);
+			}
+			if(country != null) {
+				if(hql.length() > queryLength)
+					hql.concat(" AND ");
+				hql.concat("C.pais = '" + country + "'");
+			}
+		}
+		
+		Query query = em.createQuery(hql);
+		if(startPeriod != null) {
+			try {
+				
+				String[] dateArr = startPeriod.split("-");
+				int year = Integer.parseInt(dateArr[0]);
+				if(year < 1900 || year > (new Date().getYear() + 1900))	//new Date().getYear() + 1900 = current year
+					throw new InvalidJSONQueryException("startPeriod.year NOT VALID");
+				int month = Integer.parseInt(dateArr[1]);
+				if(month < 1 || month > 12)
+					throw new InvalidJSONQueryException("startPeriod.month NOT VALID");
+				int day = Integer.parseInt(dateArr[2]);
+				if(day < 1 || day > 31)
+					throw new InvalidJSONQueryException("startPeriod.day NOT VALID");
+				Date spDate = new Date(year, month, day);			// Deprecated -> Cambiar tipo a Calendar? TODO
+				query.setParameter("startPeriod", spDate);
+				
+			}catch(NullPointerException e) {
+				throw new InvalidJSONQueryException("startPeriod NOT VALID");
+			}
+		}
+		if(endPeriod != null) {
+			try {
+				
+				String[] dateArr = endPeriod.split("-");
+				int year = Integer.parseInt(dateArr[0]);
+				if(year < 1900 || year > (new Date().getYear() + 1900))	//new Date().getYear() + 1900 = current year
+					throw new InvalidJSONQueryException("endPeriod.year NOT VALID");
+				int month = Integer.parseInt(dateArr[1]);
+				if(month < 1 || month > 12)
+					throw new InvalidJSONQueryException("endPeriod.month NOT VALID");
+				int day = Integer.parseInt(dateArr[2]);
+				if(day < 1 || day > 31)
+					throw new InvalidJSONQueryException("endPeriod.day NOT VALID");
+				Date epDate = new Date(year, month, day);	// Deprecated -> Cambiar tipo a Calendar? TODO
+				query.setParameter("endPeriod", epDate);
+				
+			}catch(NullPointerException e) {
+				throw new InvalidJSONQueryException("endPeriod NOT VALID");
+			}
+		}
+		@SuppressWarnings("unchecked")
+		List<Object> results = query.getResultList();
+		return results;
 	}
 
 	@Override
