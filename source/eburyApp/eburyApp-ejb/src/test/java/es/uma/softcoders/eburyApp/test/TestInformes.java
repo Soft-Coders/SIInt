4db9c609ef1f530d00dc60b8e51c1fbbbff5c85a
jesuscestino <jesuscestino@uma.es>
2022-04-28 10:08:04 +0200

    Cambios sobre los que trabajar hoy

--- source/eburyApp/eburyApp-ejb/src/test/java/es/uma/softcoders/eburyApp/test/TestInformes.java
+++ source/eburyApp/eburyApp-ejb/src/test/java/es/uma/softcoders/eburyApp/test/TestInformes.java
@@ -1,12 +1,25 @@
+package es.uma.softcoders.eburyApp.test;
+
+import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Map;
+
 import javax.naming.NamingException;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
+import org.junit.Before;
 import org.junit.Test;
+import org.omg.PortableInterceptor.SUCCESSFUL;
 
+import es.BaseDatosInformes;
+import es.uma.softcoders.eburyApp.Empresa;
+import es.uma.softcoders.eburyApp.PersonaAutorizada;
 import es.uma.softcoders.eburyApp.ejb.GestionInformes;
 import es.uma.softcoders.eburyApp.ejb.InformesEJB;
+import es.uma.softcoders.eburyApp.exceptions.InvalidJSONQueryException;
 
 public class TestInformes {
 
@@ -17,52 +30,39 @@ public class TestInformes {
     @Before
     public void setup() throws NamingException {
         gestionInformes = (GestionInformes) new InformesEJB();
-        BaseDatosCT.inicializaBaseDatos(UNIDAD_PERSISTENCIA_PRUEBAS);
+        BaseDatosInformes.inicializaBaseDatos(UNIDAD_PERSISTENCIA_PRUEBAS);
     }
 
     @Test
     public void testInformeHolanda(){
-
-        
-            Date pDay = new Date(2017,06,23);
-            Individual pInd = new Individual("Pep", "Doe");
-            Divisa pDiv = new Divisa("GBP", "libras", '£', (long)0.94);
-            CuentaReferencia pRef = new CuentaReferencia("CaixaBank", (long)40000, pDiv);
-            Segregada pSeg = new Segregada("30",pRef);
-            List<CuentaFintech> pCuentas = pInd.getCuentas();
-            // Setters
-            pInd.setIdentificacion("Ide");
-            pInd.setTipo_cliente("INDIVIDUAL");
-            pInd.setEstado("ACTIVO");
-            pInd.setFecha_Alta(pDay);
-            pInd.setDireccion("54");
-            pInd.setCiudad("Amsterdam");
-            pInd.setCodigoPostal("7207KE");
-            pInd.setPais("NL");
-            pCuentas.add(pSeg);
-            // Persist
-            em.persist(pClient);
             
             // PRIMER TEST
-        String q1="{
-            "searchParameters": {
-            "questionType": "Customer",
-            "startPeriod": "2015-04-25",
-            "endPeriod": "2020-04-25",
-            "name": {
-            "firstName": "Pep",
-            "lastName": "Doe"
-            },
-           "address": {
-            "Number": "54",
-            "postalCode": "7207KE",
-            "country": "NL"
-            }
-            }
-            ";
+            JSONObject json = new JSONObject();
+            JSONObject sP = new JSONObject();
+            JSONObject addr = new JSONObject();
+            JSONObject name = new JSONObject();
+            name.put("firstName", "Pep");
+            name.put("lastName", "Doe");
+
+            addr.put("Number", "54");
+            addr.put("postalCode", "7207KE");
+            addr.put("country", "NL");
+
+            sP.put("questionType", "Customer");
+            sP.put("startPeriod", "2015-04-25");
+            sP.put("endPeriod", "2020-04-25");
+            sP.put("adress", addr);
+            sP.put("name",name);
+
+            json.put("searchParameters", sP);
+            
+            String query = JSONValue.toJSONString(json);
+
+
+
             try{
-                List<Object> pRes = gestionInformes.informeHolanda(q1);
-                if(Res.isEmpty())
+                List<Object> pRes = gestionInformes.informeHolanda(query);
+                if(pRes.isEmpty())
                 {
                     fail("No deberia ser vacia");
                 }
@@ -73,31 +73,88 @@ public class TestInformes {
             }
 
             // SEGUNDA PRUEBA
-            String q2="{
-                "searchParameters": {
-                "questionTipe": "Customer",
-                "startPeriodo": "2015-04-25",
-                "endPeriodo": "2020-04-25",
-                "namae": {
-                "firstName": "Pep",
-                "lastName": "Doe"
+            JSONObject json2 = new JSONObject();
+            JSONObject sP2 = new JSONObject();
+            JSONObject addr2 = new JSONObject();
+            JSONObject name2 = new JSONObject();
+            name2.put("firstName", "Pep");
+            name2.put("lastName", "Doe");
+
+            addr2.put("Number", "54");
+            addr2.put("postalCode", "7207KE");
+            addr2.put("country", "NL");
+
+            sP2.put("questionTipe", "Customer");
+            sP2.put("startPeriodo", "2015-04-25");
+            sP2.put("endPeriodo", "2020-04-25");
+            sP2.put("adress", addr2);
+            sP2.put("namae",name2);
+
+            json2.put("searchParameters", sP2);
+            
+            query = JSONValue.toJSONString(json2);
+            
+            /*
+            query="{
+                \"searchParameters\": {
+                \"questionTipe\": \"Customer\",
+                \"startPeriodo\": \"2015-04-25\",
+                \"endPeriodo\": \"2020-04-25\",
+                \"namae\": {
+                \"firstName\": \"Pep\",
+                \"lastName\": \"Doe\"
                 },
-            "address": {
-                "Number": "54",
-                "postalCode": "7207KE",
-                "country": "NL"
+            \"address\": {
+                \"Number\": \"54\",
+                \"postalCode\": \"7207KE\",
+                \"country\": \"NL\"
                 }
-            }"; // ALGUNAS LINEAS DEL JSON INVALIDAS
+            }";
+            */ // ALGUNAS LINEAS DEL JSON INVALIDAS
 
             try{
-                List<Object> pRes = gestionInformes.informeHolanda(q2);
+                List<Object>pRes = gestionInformes.informeHolanda(query);
                 
             }catch(InvalidJSONQueryException e){
-                // en caso de que sea success catch y ya?
+                
             }
 
-        String q3="";
-        String q4="";
+            // TEST CON CLIENTE EMPRESA
+            
+            JSONObject json3 = new JSONObject();
+            JSONObject sP3 = new JSONObject();
+            JSONObject addr3 = new JSONObject();
+            JSONObject name3 = new JSONObject();
+            name3.put("firstName", "Pep");
+            name3.put("lastName", "Doe");
+
+            addr3.put("Number", "54");
+            addr3.put("postalCode", "7207KE");
+            addr3.put("country", "NL");
+
+            sP3.put("questionType", "Customer");
+            sP3.put("startPeriod", "2015-04-25");
+            sP3.put("endPeriod", "2020-04-25");
+            sP3.put("adress", addr3);
+            sP3.put("name",name3);
+
+            json3.put("searchParameters", sP3);
+            
+            query = JSONValue.toJSONString(json3);
+            
+            // COMO SERÍA LA QUERY PARA EMPRESAS?
+
+            try{
+                List<Object> pRes = gestionInformes.informeHolanda(query);
+                if(pRes.isEmpty())
+                {
+                    fail("No deberia ser vacia");
+                }
+            }catch(NullPointerException | InvalidJSONQueryException e){
+                    fail("No debe dar error");
+            }
+            
+            query="";
 
 
     }
