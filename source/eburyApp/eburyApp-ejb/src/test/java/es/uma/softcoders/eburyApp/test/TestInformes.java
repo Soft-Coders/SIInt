841179f90f73129b692ee4eea50afecd2d265772
CestinoJesus <gonzalez2@hotmail.es>
2022-04-28 07:12:12 +0200

    Siguiente version de los tests

--- source/eburyApp/eburyApp-ejb/src/test/java/es/uma/softcoders/eburyApp/test/TestInformes.java
+++ source/eburyApp/eburyApp-ejb/src/test/java/es/uma/softcoders/eburyApp/test/TestInformes.java
@@ -16,6 +16,28 @@ public class TestInformes {
 
     @Test
     public void testInformeHolanda(){
+
+        
+            Date pDay = new Date(2017,06,23);
+            Individual pInd = new Individual("Pep", "Doe");
+            Divisa pDiv = new Divisa("GBP", "libras", '£', (long)0.94);
+            CuentaReferencia pRef = new CuentaReferencia("CaixaBank", (long)40000, pDiv);
+            Segregada pSeg = new Segregada("30",pRef);
+            List<CuentaFintech> pCuentas = pInd.getCuentas();
+            // Setters
+            pInd.setIdentificacion("Ide");
+            pInd.setTipo_cliente("INDIVIDUAL");
+            pInd.setEstado("ACTIVO");
+            pInd.setFecha_Alta(pDay);
+            pInd.setDireccion("54");
+            pInd.setCiudad("Amsterdam");
+            pInd.setCodigoPostal("7207KE");
+            pInd.setPais("NL");
+            pCuentas.add(pSeg);
+            // Persist
+            em.persist(pClient);
+            
+            // PRIMER TEST
         String q1="{
             "searchParameters": {
             "questionType": "Customer",
@@ -30,18 +52,47 @@ public class TestInformes {
             "postalCode": "7207KE",
             "country": "NL"
             }
-           }
-           ";
-           Date pDay = new Date(2017,06,23);
-           Cliente pClient = new Cliente("ident","INDIVIDUAL","ACTIVO",pDay,);
+            }
+            ";
             try{
-
+                List<Object> pRes = gestionInformes.informeHolanda(q1);
+                if(Res.isEmpty())
+                {
+                    fail("No deberia ser vacia");
+                }
             
-            }catch(){
+            }catch(NullPointerException | InvalidJSONQueryException e){
+                fail("No debería de lanzar esta excepcion");
 
-           }
-        String q2="";
+            }
+
+            // SEGUNDA PRUEBA
+            String q2="{
+                "searchParameters": {
+                "questionTipe": "Customer",
+                "startPeriodo": "2015-04-25",
+                "endPeriodo": "2020-04-25",
+                "namae": {
+                "firstName": "Pep",
+                "lastName": "Doe"
+                },
+            "address": {
+                "Number": "54",
+                "postalCode": "7207KE",
+                "country": "NL"
+                }
+            }"; // ALGUNAS LINEAS DEL JSON INVALIDAS
+
+            try{
+                List<Object> pRes = gestionInformes.informeHolanda(q2);
+                
+            }catch(InvalidJSONQueryException e){
+                // en caso de que sea success catch y ya?
+            }
+
         String q3="";
+        String q4="";
+
 
     }
 
